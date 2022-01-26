3a2c212ad9793761dcb8810f9e35fa4b8a2cb30b
fulekylaszlo <fulekylaszlo@gmail.com>
2022-01-26 08:56:25 +0100

    Added custom gitignore

--- bombcrypto.py
+++ bombcrypto.py
@@ -5,10 +5,11 @@ from datetime import datetime
 
 webbrowser.open('https://app.bombcrypto.io/?_ga=2.111689284.1794165064.1637188846-360359701.1636966379&_gl=1*1y3vvzp*_ga*MzYwMzU5NzAxLjE2MzY5NjYzNzk.*_ga_JECCFV3TTN*MTYzNzE4ODg0Ni4yLjAuMTYzNzE4ODg0Ni4w')
 now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+timer_count = 0
 
 location_connect_wallet = None
 while location_connect_wallet is None:
-    location_connect_wallet = pyautogui.locateCenterOnScreen('images/connect_wallet.png', grayscale = True, confidence = 0.9)
+    location_connect_wallet = pyautogui.locateCenterOnScreen('images/connect_wallet.png', grayscale = True, confidence = 0.8)
 print('connect_wallet_found')
 pyautogui.doubleClick(location_connect_wallet, interval = 1)
 
@@ -27,7 +28,7 @@ print('hero_button_found')
 location_work_all = None
 while location_work_all is None:
     location_work_all = pyautogui.locateCenterOnScreen('images/work_all.png', grayscale = True, confidence = 0.9)
-time.sleep(5)
+time.sleep(3)
 pyautogui.doubleClick(location_work_all, interval = 1)
 print('work_all_found')
 
@@ -44,7 +45,7 @@ while location_treasure_hunt is None:
     pyautogui.click(location_treasure_hunt)
 print('treasure_hunt_found')
 
-time.sleep(5)
+time.sleep(3)
 
 location_chest = None
 while location_chest is None:
@@ -52,9 +53,35 @@ while location_chest is None:
     pyautogui.click(location_chest)
 
     while True:
-        time.sleep(2)
+        time.sleep(1)
         pyautogui.screenshot('coin_info/' + "coin_status_" + now + ".png")
         break
 
 print('chest_found')
 print('screenshot_saved')
+
+time.sleep(2)
+location_close_menu = None
+while location_close_menu is None:
+    location_close_menu = pyautogui.locateCenterOnScreen('images/close_menu.png', grayscale = True, confidence = 0.9)
+    pyautogui.click(location_close_menu)
+print('close_menu_found')
+
+def timer():
+    time.sleep(10)
+    location_back_arrow = None
+    while location_back_arrow is None:
+        location_back_arrow = pyautogui.locateCenterOnScreen('images/back_arrow.png', grayscale = True, confidence = 0.9)
+        pyautogui.click(location_back_arrow)
+        print('back_arrow_found')
+    print('Waited')
+    time.sleep(2)
+    location_treasure_hunt = None
+    while location_treasure_hunt is None:
+        location_treasure_hunt = pyautogui.locateCenterOnScreen('images/treasure_hunt.png', grayscale = True, confidence = 0.9)
+        pyautogui.click(location_treasure_hunt)
+    print('treasure_hunt_found')
+
+while timer_count < 5:
+    timer()
+    timer_count += 1
