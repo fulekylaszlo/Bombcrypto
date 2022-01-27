0eccd4179f8c29936917573894b35a9bc938c7c7
fulekylaszlo <fulekylaszlo@gmail.com>
2022-01-27 08:22:03 +0100

    Added sleep function to heroes

--- bombcrypto.py
+++ bombcrypto.py
@@ -7,6 +7,7 @@ webbrowser.open('https://app.bombcrypto.io/?_ga=2.111689284.1794165064.163718884
 now = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
 timer_count = 0
 
+
 location_connect_wallet = None
 while location_connect_wallet is None:
     location_connect_wallet = pyautogui.locateCenterOnScreen('images/connect_wallet.png', grayscale = True, confidence = 0.8)
@@ -44,7 +45,6 @@ while location_treasure_hunt is None:
     location_treasure_hunt = pyautogui.locateCenterOnScreen('images/treasure_hunt.png', grayscale = True, confidence = 0.9)
     pyautogui.click(location_treasure_hunt)
 print('treasure_hunt_found')
-
 time.sleep(3)
 
 location_chest = None
@@ -54,7 +54,7 @@ while location_chest is None:
 
     while True:
         time.sleep(1)
-        pyautogui.screenshot('coin_info/' + "coin_status_" + now + ".png")
+        pyautogui.screenshot('coin_info/' + "coin_status_start_" + now + ".png")
         break
 
 print('chest_found')
@@ -68,7 +68,7 @@ while location_close_menu is None:
 print('close_menu_found')
 
 def timer():
-    time.sleep(10)
+    time.sleep(300)
     location_back_arrow = None
     while location_back_arrow is None:
         location_back_arrow = pyautogui.locateCenterOnScreen('images/back_arrow.png', grayscale = True, confidence = 0.9)
@@ -82,6 +82,47 @@ def timer():
         pyautogui.click(location_treasure_hunt)
     print('treasure_hunt_found')
 
-while timer_count < 5:
+while timer_count < 6:
     timer()
     timer_count += 1
+
+location_back_arrow = None
+while location_back_arrow is None:
+    location_back_arrow = pyautogui.locateCenterOnScreen('images/back_arrow.png', grayscale = True, confidence = 0.9)
+    pyautogui.click(location_back_arrow)
+    print('back_arrow_found')
+
+location_hero_button = None
+while location_hero_button is None:
+    location_hero_button = pyautogui.locateCenterOnScreen('images/hero_button.png', grayscale= True,  confidence = 0.9)
+pyautogui.doubleClick(location_hero_button)
+print('hero_button_found')
+
+location_sleep_all = None
+while location_sleep_all is None:
+    location_sleep_all = pyautogui.locateCenterOnScreen('images/sleep_all.png', grayscale = True, confidence = 0.9)
+time.sleep(2)
+pyautogui.doubleClick(location_sleep_all, interval = 1)
+print('sleep_all_found')
+time.sleep(1)
+
+location_close_menu = None
+while location_close_menu is None:
+    location_close_menu = pyautogui.locateCenterOnScreen('images/close_menu.png', grayscale = True, confidence = 0.9)
+    pyautogui.click(location_close_menu)
+print('close_menu_found')
+time.sleep(1)
+
+
+location_chest = None
+while location_chest is None:
+    location_chest = pyautogui.locateCenterOnScreen('images/chest.png', grayscale = True, confidence = 0.9)
+    pyautogui.click(location_chest)
+
+    while True:
+        time.sleep(1)
+        pyautogui.screenshot('coin_info/' + "coin_status_end_" + now + ".png")
+        break
+
+print('chest_found')
+print('screenshot_saved')
